a2246ef2dced5bdbca5493dc72a0caf70e30b480
Roy <jh0292jh@gmail.com>
2023-04-09 00:49:31 -0400

    UpdateSecondPage

--- lib/screens/next_screen.dart
+++ lib/screens/next_screen.dart
@@ -6,17 +6,22 @@ class SecondScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Second Route'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('Go back!'),
+        appBar: AppBar(
+          title: const Text('Second Route'),
         ),
-      ),
-    );
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('Go back!'),
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Container(
+            height: 40,
+            color: Colors.black,
+          ),
+        ));
   }
 }
