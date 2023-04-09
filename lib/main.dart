e6d0ea651b72d6346acba0e6c2d9eaf2caa71fd4
Roy <jh0292jh@gmail.com>
2023-04-09 00:42:24 -0400

    first

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import './screens/next_screen.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    title: "NavigationBasic",
+    home: FirstScreen(),
+  ));
+}
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("First Page"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text("Hi"),
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => SecondScreen()));
+          },
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(),
+    );
+  }
+}
